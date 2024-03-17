14347cd887ed885589f584c25365b17e527280fc
thujuli <thujuli037@gmail.com>
2024-03-17 23:08:45 +0700

    feat: Introduce new section and components to home page
    - Create components Footer and TestimonialCard
    - Add new section Testimonials to the home page
    - Update property "border" on ButtonCircle to resolve a bug
    - Update color on ServiceCard

---
+++ src/components/Footer.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Container from "./Container";
+import { menuItems } from "@/utils/helper";
+import Link from "next/link";
+import { MdCall, MdLocationPin, MdMail } from "react-icons/md";
+import { FaInstagram, FaTwitter, FaWhatsapp, FaYoutube } from "react-icons/fa";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-secondary/20 pt-[100px]">
+      <Container>
+        <div className="flex gap-10">
+          <div className="w-1/2">
+            <h3 className="mb-4 text-2xl font-bold">Kreasi Digital</h3>
+            <p className="text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              porro vitae qui possimus hic soluta, omnis dolores nemo assumenda
+              aliquid nostrum cumque unde maxime labore. Odit suscipit fugiat
+              assumenda accusamus.
+            </p>
+          </div>
+          <div className="grid grid-cols-3 gap-6">
+            <div className="flex flex-col gap-4">
+              <span className="text-xl font-medium">Kreasi Digital</span>
+              {menuItems.map((menu, idx) => (
+                <Link key={idx} href={menu.url} className="text-secondary">
+                  {menu.name}
+                </Link>
+              ))}
+            </div>
+            <div className="flex flex-col gap-4">
+              <span className="text-xl font-medium">Contact</span>
+              <span className="flex items-center gap-2 text-secondary">
+                <MdCall className="text-xl text-white" /> (406) 555-0120
+              </span>
+              <span className="flex items-center gap-2 text-secondary">
+                <MdMail className="text-xl text-white" />{" "}
+                kreasi.digital@gmail.com
+              </span>
+              <span className="flex items-center gap-2 text-secondary">
+                <MdLocationPin className="text-xl text-white" /> 2972 Westheimer
+                Rd. Santa Ana, Ilinois 85486
+              </span>
+            </div>
+            <div className="flex flex-col gap-4">
+              <span className="text-xl font-medium">Social</span>
+              <span className="flex items-center gap-2 text-secondary">
+                <FaInstagram className="text-xl text-white" /> Instagram
+              </span>
+              <span className="flex items-center gap-2 text-secondary">
+                <FaWhatsapp className="text-xl text-white" /> Whatsapp
+              </span>
+              <span className="flex items-center gap-2 text-secondary">
+                <FaYoutube className="text-xl text-white" /> Youtube
+              </span>
+              <span className="flex items-center gap-2 text-secondary">
+                <FaTwitter className="text-xl text-white" /> Twitter
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8 pb-6">
+          <p className="text-sm text-end text-secondary">
+            &copy; {new Date().getFullYear()} Kreasi Digital. All rights
+            reserved.
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
