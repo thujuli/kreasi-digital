0848a79b435ed8954f78d4aaefad3c64c56a15e5
thujuli <thujuli037@gmail.com>
2024-03-22 02:24:12 +0700

    feat: Retrieve success projects data from contentful
    - Introduce utilities to fetch entries from contentful
    - Remove list success projects data from helper
    - Remove images related to success projects data
    - Configure next.config.mjs to allow external image links

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "randomuser.me",
+        hostname: "**",
         port: "",
         pathname: "**",
       },
---
+++ src/utils/types/projects.ts
@@ -0,0 +1,7 @@
+export interface ProjectData {
+  title: string;
+  description: string;
+  job: string;
+  projectLink: string;
+  imageUrl: string;
+}
